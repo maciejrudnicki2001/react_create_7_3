a1b2f8710ec9acd2b52c50626c2de1866bd2b82e
maciejrudnicki2001 <s22983@pjwstk.edu.pl>
2022-12-04 11:10:59 +0100

    done
    done

---
+++ src/index.js
@@ -0,0 +1,22 @@
+import React,{StrictMode} from "react";
+import { createRoot } from "react-dom/client";
+
+import Header from "./components/Header";
+import Section from "./components/Section";
+import Footer from "./components/Footer";
+import Article from "./components/Article";
+import Nav from "./components/Navbar";
+
+import "./style/index.css";
+
+const root = createRoot(document.getElementById("root"))
+root.render(
+    <StrictMode>
+        <header>
+            <p>Navigation bar</p>
+            <Header />
+        </header>
+        <Section />
+        <Footer />
+    </StrictMode>
+);
